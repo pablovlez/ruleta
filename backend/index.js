94bb3c2378fe7dec4c906a22942607a9e1d16615
pablovlez <pablo.vlez@gmail.com>
2019-05-26 14:35:57 -0500

    preparando para subir a heroku

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
 if(process.env.NODE_ENV === "development"){
     require('dotenv').config();
 }
@@ -22,7 +34,7 @@ const storage = multer.diskStorage({
     }
 })
 
-app.use(multer({storage}).single('image'));
+//app.use(multer({storage}).single('image'));
 app.use(express.urlencoded({extended: false})); //recive JSON from form
 app.use(express.json()); //recive JSON native
 app.use(cors());
