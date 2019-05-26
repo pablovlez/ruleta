2476101a1fa879b444d8bfb78124b3aa72daa425
pablovlez <pablo.vlez@gmail.com>
2019-05-26 15:01:45 -0500

    preparando para subir a heroku

--- backend/routes/personas.js
+++ backend/routes/personas.js
@@ -13,24 +13,48 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {    
-    const {nombre, dinero } = req.body;    
-    const newPersona = new Persona({nombre, dinero});    
-    await newPersona.save();
-    res.json({message:'Persona almacenada con exito'});
+    try {
+        const {nombre, dinero } = req.body;
+        console.log(req);
+        const newPersona = new Persona({nombre, dinero});    
+        await newPersona.save();
+        res.json({message:'Persona almacenada con exito'});
+    } catch (error) {
+        res.json({
+            message:'Ha ocurrido un error',
+            error
+        });
+    }
+    
 });
 
 router.delete('/:id', async (req, res) => {
-    const persona = await Persona.findByIdAndDelete(req.params.id);
-    //unlink(path.resolve('./backend/public' + persona.imagePath));
-    res.json({message: 'Persona eliminada con exito'});
+    try {
+        const persona = await Persona.findByIdAndDelete(req.params.id);        
+        res.json({message: 'Persona eliminada con exito'});
+    } catch (error) {
+        res.json({
+            message:'Ha ocurrido un error',
+            error 
+        });
+    }
+    
 });
 
 router.put('/:id', async (req, res) => {
-    const {nombre, dinero } = req.body;
-    const persona = await Persona.findByIdAndUpdate(req.params.id,
+    try {
+        const {nombre, dinero } = req.body;
+        const persona = await Persona.findByIdAndUpdate(req.params.id,
         { nombre, dinero }
-    );    
-    res.json({message: 'Persona actualizada con exito'});
+        );    
+        res.json({message: 'Persona actualizada con exito'});
+    } catch (error) {
+        res.json({
+            message:'Ha ocurrido un error',
+            error 
+        });
+    }
+    
 });
 
 router.get('/:id', async (req, res) => {
